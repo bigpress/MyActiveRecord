d93c91f78a2cf619b695362f428c686203bcf2ac
Ramon Antonio Parada <ramon@bigpress.net>
2011-06-14 10:19:52 +0200

    clean

---
+++ config.sample.php
@@ -0,0 +1,10 @@
+<?php
+
+
+
+define("MYACTIVERECORD_CACHE_SQL", true);
+
+define("SQL_HOST", "127.0.0.1"); //no dns, faster
+define("SQL_LOGIN", "something");
+define("SQL_PASSE", "secret");
+define("SQL_DBASE", "dbname");
